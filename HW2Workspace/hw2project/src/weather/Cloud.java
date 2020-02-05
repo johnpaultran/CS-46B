5e3cd544fcd1b9f2fbc7936039ef72cff4c87436
John Paul Tran <56181958+johnpaultran@users.noreply.github.com>
2020-02-05 15:29:37 -0800

    Add files via upload

---
+++ HW2Workspace/hw2project/src/weather/Cloud.java
@@ -0,0 +1,24 @@
+package weather;
+
+public class Cloud 
+{
+	private float bottom;
+	private float top;
+	
+	public Cloud(float bottom, float top)
+	{
+		this.bottom = bottom;
+		this.top = top;
+	}
+	
+	public float getHeight()
+	{
+		float height = top - bottom;
+		return height;
+	}
+	
+	public String rain()
+	{
+		return "It is raining";
+	}
+}
